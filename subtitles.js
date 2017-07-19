3793883b2893deca70556d5847520cb05dba6768
Sylas Eckart Macena <eckartd.sylas@gmail.com>
2017-07-19 11:11:08 -0300

    initial configuration

---
+++ subtitles.js
@@ -0,0 +1,23 @@
+const OpenSubtitles = require('opensubtitles-api')
+, os = new OpenSubtitles('OSTestUserAgentTemp')
+, decompress = require('decompress')
+, http = require('http')
+, fs = require('fs');
+
+module.exports = {
+    init: function(file,path){
+        os
+        .search({filename: file+'.mkv',sublanguageid: 'pob'})
+        .then(subtitles => {
+            if (subtitles.pb) {
+                console.log('Subtitle found:', subtitles);
+                let episode = fs.createWriteStream(`${file}.srt`);
+                http.get(subtitles.pb.url,response =>{response.pipe(episode)})
+            }
+            else {
+                console.log(`Não achei legenda`);
+            }
+        })          
+        .catch(err => console.log(err));
+    }
+}
